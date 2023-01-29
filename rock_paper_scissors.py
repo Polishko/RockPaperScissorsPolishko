76b5e1604c2fa8d1d9c7526d18629c653ad92699
Niya Mateeva (Nalan Bilgin) <119063181+Polishko@users.noreply.github.com>
2023-01-29 17:58:07 +0200

    Updated file
    Corrected code

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -2,48 +2,52 @@ import random
 rock = "Rock"
 paper = "Paper"
 scissors = "Scissors"
-
-player_move = input("Choose [r]ock, [p]aper or [s]cissors: ")
-
-if player_move == "r":
-    player_move = rock
-elif player_move == "p":
-    player_move = paper
-elif player_move == "s":
-    player_move = scissors
-else:
-    raise SystemExit("Invalid Input. Try again...")
-
-computer_random_number = random.randint(1, 3)
-computer_move = ""
-
-your_won_games = 0
-computer_won_games = 0
-
-if computer_random_number == 1:
-    computer_move = rock
-elif computer_random_number == 2:
-    computer_move = paper
-else:
-    computer_move = scissors
-
-print(f"The computer chose {computer_move}.")
+your_won_games, computer_won_games = 0, 0
 
 while True:
+    player_move = input("Choose [r]ock, [p]aper or [s]cissors: ")
 
-    if (player_move == rock and computer_move == scissors) or \
-            (player_move == scissors and computer_move == paper) or \
+    if player_move == "r":
+        player_move = rock
+    elif player_move == "p":
+        player_move = paper
+    elif player_move == "s":
+        player_move = scissors
+    else:
+        print("Invalid Input. Try again...")
+        continue
+
+    computer_random_number = random.randint(1, 3)
+    computer_move = ""
+
+    if computer_random_number == 1:
+        computer_move = rock
+    elif computer_random_number == 2:
+        computer_move = paper
+    else:
+        computer_move = scissors
+
+    print(f"The computer chose {computer_move}.")
+
+    if (player_move == rock and computer_move == scissors) or (player_move == scissors and computer_move == paper) or \
             (player_move == paper and computer_move == rock):
         your_won_games += 1
         print("You win this hand!")
+        print(f"Your points: {your_won_games}")
+        print(f"Computer points: {your_won_games}")
     elif player_move == computer_move:
         print("Draw!")
     else:
         computer_won_games += 1
-        print("You lose this hand!")
+        print("The computer wins this hand!")
+        print(f"Your points: {your_won_games}")
+        print(f"Computer points: {your_won_games}")
 
     if your_won_games == 5:
         print("You win this game!")
+        break
     elif computer_won_games == 5:
         print("You lose this game!")
-    break
+        break
+
+print("Thank you for playing!")
